39c7dcc59ed40f679ece6b171ec011d30cf5cca9
Віталій П'ятницький <Vitalii.Piatnytskyi.PZ.2018@edu.lpnu.ua>
2021-11-25 12:38:00 +0200

    Provided RSA keys parameters.

--- RSA/RSAWrapper.cs
+++ RSA/RSAWrapper.cs
@@ -4,44 +4,54 @@ namespace RSA
 {
     public class RSAWrapper
     {
-        private readonly RSACryptoServiceProvider _rsaCryptoServiceProvider;
         private readonly int _blockSize;
+        private readonly bool _doOAEPPadding;
 
-        public RSAWrapper(RSACryptoServiceProvider rsaCryptoServiceProvider, int blockSize)
+        public RSAWrapper(int blockSize, bool doOAEPPadding)
         {
-            _rsaCryptoServiceProvider = rsaCryptoServiceProvider;
             _blockSize = blockSize;
+            _doOAEPPadding = doOAEPPadding;
         }
 
-        public async Task Encrypt(Stream unencrypted, Stream encrypted)
+        public async Task Encrypt(Stream unencrypted, Stream encrypted, RSAParameters rsaKeyInfo)
         {
-            for (
-                int currentPosition = 0;
-                currentPosition < unencrypted.Length;
-                currentPosition += _blockSize)
+            using (var rsaCryptoServiceProvider = new RSACryptoServiceProvider())
             {
-                var inputBlock = new byte[_blockSize];
-                unencrypted.Read(inputBlock, currentPosition, _blockSize);
+                rsaCryptoServiceProvider.ImportParameters(rsaKeyInfo);
 
-                await encrypted.WriteAsync(_rsaCryptoServiceProvider.Encrypt(
-                    inputBlock,
-                    fOAEP: false));
+                for (
+                    int currentPosition = 0;
+                    currentPosition < unencrypted.Length;
+                    currentPosition += _blockSize)
+                {
+                    var inputBlock = new byte[_blockSize];
+                    unencrypted.Read(inputBlock, currentPosition, _blockSize);
+
+                    await encrypted.WriteAsync(rsaCryptoServiceProvider.Encrypt(
+                        inputBlock,
+                        _doOAEPPadding));
+                }
             }
         }
 
-        public async Task Decipher(Stream encrypted, Stream unencrypted)
+        public async Task Decipher(Stream encrypted, Stream unencrypted, RSAParameters rsaKeyInfo)
         {
-            for (
-                int currentPosition = 0;
-                currentPosition < encrypted.Length;
-                currentPosition += _blockSize)
+            using (var rsaCryptoServiceProvider = new RSACryptoServiceProvider())
             {
-                var inputBlock = new byte[_blockSize];
-                encrypted.Read(inputBlock, currentPosition, _blockSize);
+                rsaCryptoServiceProvider.ImportParameters(rsaKeyInfo);
 
-                await unencrypted.WriteAsync(_rsaCryptoServiceProvider.Decrypt(
-                    inputBlock,
-                    fOAEP: false));
+                for (
+                    int currentPosition = 0;
+                    currentPosition < encrypted.Length;
+                    currentPosition += _blockSize)
+                {
+                    var inputBlock = new byte[_blockSize];
+                    encrypted.Read(inputBlock, currentPosition, _blockSize);
+
+                    await unencrypted.WriteAsync(rsaCryptoServiceProvider.Decrypt(
+                        inputBlock,
+                        _doOAEPPadding));
+                }
             }
         }
     }
